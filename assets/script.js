6469cbdc93f609938a7e8ec0be4f0f2fc7a1e222
Ryan Wilson <ryanswilson1@gmail.com>
2020-10-05 16:41:22 -0400

    updated HTML and JS

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,26 @@
 $(document).ready(function () {
-    var currentDay = new Date();
-    var momentDay = moment(currentDay).format('dddd, MMMM Do');
-    console.log(momentDay);
 
-});
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+
+
+    $(document).on("click", "i", function (event) {
+        event.preventDefault();
+        console.log(this);
+        var word = $(".input").val();
+        var clock = $(".hour").text();
+        console.log(word);
+        console.log(clock);
+
+        localStorage.setItem(clock, word);
+    })
+
+    $("#9AM .description").val(localStorage.getItem("9AM"));
+    $("#10AM .description").val(localStorage.getItem("10AM"));
+    $("#11AM .description").val(localStorage.getItem("11AM"));
+    $("#12PM .description").val(localStorage.getItem("12PM"));
+    $("#1PM .description").val(localStorage.getItem("1PM"));
+    $("#2PM .description").val(localStorage.getItem("2PM"));
+    $("#3PM .description").val(localStorage.getItem("3PM"));
+    $("#4PM .description").val(localStorage.getItem("4PM"));
+    $("#5PM .description").val(localStorage.getItem("5PM"));
+})
