b9a33da01b6b847ab16dda6f71ac910e76f45678
Ryan Wilson <ryanswilson1@gmail.com>
2020-10-07 13:07:04 -0400

    updated fill function

--- assets/script.js
+++ assets/script.js
@@ -7,3 +7,16 @@ var two = localStorage.getItem("2PM") || "";
 var three = localStorage.getItem("3PM") || "";
 var four = localStorage.getItem("4PM") || "";
 var five = localStorage.getItem("5PM") || "";
+
+
+function fill() {
+    $("#9AM").children(".input").val(nine);
+    $("#10AM").children(".input").val(ten);
+    $("#11AM").children(".input").val(eleven);
+    $("#12PM").children(".input").val(twelve);
+    $("#1PM").children(".input").val(one);
+    $("#2PM").children(".input").val(two);
+    $("#3PM").children(".input").val(three);
+    $("#4PM").children(".input").val(four);
+    $("#5PM").children(".input").val(five);
+};
